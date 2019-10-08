1f9a9e797d1e80acb92d21578ad330d9151fe683
Rodrigo <rodri_arganda@hotmail.com>
2019-10-08 15:46:30 +0200

    creacion proyecto

--- 08_EjercicioAngular/gestionApp/README.md
+++ 08_EjercicioAngular/gestionApp/README.md
@@ -1,4 +1,4 @@
-# Aplicacion
+# GestionApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.8.
 
--- 08_EjercicioAngular/gestionApp/src/app/app.component.spec.ts
+++ 08_EjercicioAngular/gestionApp/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'aplicacion'`, () => {
+  it(`should have as title 'gestionApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('aplicacion');
+    expect(app.title).toEqual('gestionApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('aplicacion app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('gestionApp app is running!');
   });
 });
