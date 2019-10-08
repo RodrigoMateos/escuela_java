a0255a570a082d396984feffef85fb98635f0646
Rodrigo <rodri_arganda@hotmail.com>
2019-10-08 15:52:15 +0200

    base completa

--- 08_EjercicioAngular/gestionApp/src/app/app.module.ts
+++ 08_EjercicioAngular/gestionApp/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RouterOutletComponent } from './router-outlet/router-outlet.component';
 import { MensajesComponent } from './mensajes/mensajes.component';
+import { RegistroComponent } from './registro/registro.component';
+import { ListadoComponent } from './listado/listado.component';
+import { FormUsuarioComponent } from './form-usuario/form-usuario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     RouterOutletComponent,
-    MensajesComponent
+    MensajesComponent,
+    RegistroComponent,
+    ListadoComponent,
+    FormUsuarioComponent
   ],
   imports: [
     BrowserModule,
